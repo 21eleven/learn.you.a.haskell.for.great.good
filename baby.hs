05f1f254590bc98fa72508b0145f3efd20666b32
Noah Lidell <noahlidell@gmail.com>
2022-07-16 18:51:22 -0500

    recursive fxns

--- baby.hs
+++ baby.hs
@@ -17,3 +17,35 @@ removeLowercaseChars str = [c | c <- str, c `elem` ['A' .. 'Z']]
 
 fact 0 = 1
 fact x = x * fact (x -1)
+
+maxx :: (Ord a) => [a] -> a
+maxx [] = error "max of an empty list attempted"
+maxx [x] = x
+maxx (x : xs)
+  | x > maxTail = x
+  | otherwise = maxTail
+  where
+    maxTail = maxx xs
+
+minn :: (Ord a) => [a] -> a
+minn [] = error "min of an empty list attemped"
+minn [x] = x
+minn (x : xs)
+  | x <= minTail = x
+  | otherwise = minTail
+  where
+    minTail = minn xs
+
+quicksort' :: (Ord a) => [a] -> [a]
+quicksort' [] = []
+quicksort' (x : xs) =
+  let smaller = quicksort' [a | a <- xs, a <= x]
+      bigger = quicksort' [a | a <- xs, a > x]
+   in smaller ++ [x] ++ bigger
+
+quicksort :: (Ord a) => [a] -> [a]
+quicksort [] = []
+quicksort (x : xs) =
+  let smallerSorted = quicksort [a | a <- xs, a <= x]
+      biggerSorted = quicksort [a | a <- xs, a > x]
+   in smallerSorted ++ [x] ++ biggerSorted
